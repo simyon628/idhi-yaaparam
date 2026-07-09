01729a2c18f835b6434229bf1aadff967248a36d
simyon628 <simyon628@gmail.com>
2026-07-09 21:22:31 +0530

    Clean up register_task.ps1
    Co-authored-by: karunajyothi2005 <karunajyothi20005@gmail.com>
    
    Co-authored-by: simyon628 <simyon628@gmail.com>

--- register_task.ps1
+++
@@ -1,5 +0,0 @@
-$action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-NoProfile -WindowStyle Hidden -ExecutionPolicy Bypass -File 'd:\idhi yaaparam\commit.ps1' -msg 'Auto daily green square commit'"
-$trigger = New-ScheduledTaskTrigger -Daily -At 11:30PM
-$settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries
-Register-ScheduledTask -TaskName "DailyGitHubCommit_IdhiYaaparam" -Action $action -Trigger $trigger -Settings $settings -Force
-Write-Host "Task successfully registered!" -ForegroundColor Green
